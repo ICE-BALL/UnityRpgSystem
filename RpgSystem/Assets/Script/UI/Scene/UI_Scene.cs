02c0da35b0e36e5cd207fde87c277e61a6181aaa
DESKTOP-3TMI3F0\오헌 <heon225@gmail.com>
2023-02-13 22:27:03 +0900

    open bag -> keycode : B

--- RpgSystem/Assets/Script/UI/Scene/UI_Scene.cs
+++ RpgSystem/Assets/Script/UI/Scene/UI_Scene.cs
@@ -10,15 +10,31 @@ public class UI_Scene : MonoBehaviour
     public GameObject _bag;
     public GameObject _more;
 
+    Action openbag = null;
+
     private void Start()
     {
         _bag = UIManager.FindChild<Button>(gameObject, "Bag");
         _more = UIManager.FindChild<Button>(gameObject, "More");
 
+        openbag -= Bag;
+        openbag += Bag;
         UIManager.BindEvent(_bag, Bag, UIManager.UIEvent.Click);
     }
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.B))
+            openbag.Invoke();
+    }
+
     void Bag(PointerEventData data)
+    {
+        UIManager.ShowInventoryUI<UI_Inven>("UI_Inven");
+        UIManager.CloseUI(gameObject);
+    }
+
+    void Bag()
     {
         UIManager.ShowInventoryUI<UI_Inven>("UI_Inven");
         UIManager.CloseUI(gameObject);
